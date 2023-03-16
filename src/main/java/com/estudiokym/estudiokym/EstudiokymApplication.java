4f67163165550699b6188178d75516ccf41b8d62
ak34549607 <ak34549607@DEKSTOP-149-2PC.hospitalbaxada.gov.ar>
2023-03-16 10:57:29 -0300

    commit inicial del proyecto en spring boot

---
+++ src/main/java/com/estudiokym/estudiokym/EstudiokymApplication.java
@@ -0,0 +1,13 @@
+package com.estudiokym.estudiokym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudiokymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudiokymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudiokym/estudiokym/EstudiokymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudiokym.estudiokym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudiokymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
